e05176a8a5b5cf597965590c1542ff40d70437d6
Ethan Fang <chafang@groupon.com>
2015-03-03 21:55:49 -0800

    Refactor project

---
+++ AwesomeRent/AwesomeRent/Classes/UploadItems/UpdateCategories/ARUpdateCategoryViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ARUpdateCategoryViewController.swift
+//  AwesomeRent
+//
+//  Created by Ethan Fang on 1/18/15.
+//  Copyright (c) 2015 awesomerent. All rights reserved.
+//
+
+import UIKit
+
+class ARUpdateCategoryViewController: UIViewController {
+    @IBOutlet var categoryLinkScrollView: UIScrollView!
+    @IBOutlet var categoryCollectionView: UICollectionView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
